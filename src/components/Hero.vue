99aea5be25b0fa101d2b9479cb8b716c17c673b0
D4riu5 <R3iji94@gmail.com>
2023-02-01 14:33:52 +0100

    v 1.1 - added staged changes (forgot to add them in the last commit)

---
+++ src/components/Hero.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    data() {
+        return {
+            logo: 'https://uploads.sitepoint.com/wp-content/uploads/2021/04/1618197067vitejs.png',
+        }
+    }
+}
+</script>
+
+<template>
+    <img class="my-2" :src="logo" alt="">
+</template>
+
+<style scoped>
+img{
+    width: 400px;
+    aspect-ratio: 1.5/1;
+}
+</style>
+
+
